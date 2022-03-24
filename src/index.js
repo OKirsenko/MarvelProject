91598454840273b800b91d50aa9fd25449819bec
OKirsenko <senka3434@gmail.com>
2022-03-24 16:24:02 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import axios from 'axios';
 import './sass/main.scss';
+const BASE_URL =
+  'https://gateway.marvel.com:443/v1/public/characters?apikey=8ccd59f9d0817709b56fd3bd6b25a9fc';
+
+function fetchHeroes() {
+  axios.get(BASE_URL).then(console.log);
+}
+fetchHeroes();
