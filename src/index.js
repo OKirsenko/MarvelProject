81e86e6750ea8efc41c17abb915ba9768c90c38d
OKirsenko <senka3434@gmail.com>
2022-03-24 22:24:57 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import './sass/main.scss';
-const BASE_URL =
-  'https://gateway.marvel.com:443/v1/public/characters?apikey=8ccd59f9d0817709b56fd3bd6b25a9fc';
+
+const BASE_URL = 'https://akabab.github.io/superhero-api/api/all.json';
 
 function fetchHeroes() {
   axios.get(BASE_URL).then(console.log);
